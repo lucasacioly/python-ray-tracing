57c2ae1bab0806451046af4ce37eea5f033178ea
Jose Lucas Acioly <lucasacioly@outlook.com>
2022-11-05 08:50:03 -0300

    v3 inputs added, v2 inputs renamed

--- components/material.py
+++ components/material.py
@@ -7,15 +7,24 @@ class Material:
     kd : float = None
     ks : float = None
     phong_exp : float = None
+
+    kr : float = None
+    kt : float = None
+    refract_idx : float = None
     
     def __init__(self, color : Color, 
-                ambient=0.05,diffuse=1.0, specular=1.0, phong=50):
+                ambient=0.05,diffuse=1.0, specular=1.0, phong=50, 
+                kr = None, kt = None, refract_idx = None):
 
         self.color = color
         self.ka = ambient
         self.kd = diffuse
         self.ks = specular
         self.phong_exp = phong
+        self.kr = kr
+        self.kt = kt
+        self.refract_idx = refract_idx
+
     
     def get_color(self):
         return self.color.vector
--- components/scene.py
+++ components/scene.py
@@ -1,4 +1,5 @@
 from basic_geom import Point, Vector3, Color
+from objects import Object
 from light import Light
 
 class Scene:
@@ -16,14 +17,16 @@ class Scene:
     obj_list : list = None
     ambient_light : Color = None
     lights : list[Light] = None
+    max_depth : int = None
 
     def __init__(self,
                 width : int, height : int,
                 cam_pixel_size : float, cam_focus_dist : float, cam_eye : Point, 
                 cam_look_at : Point, cam_up : Vector3,
                 bg_color : Color,
-                obj_list : list,
-                ambient_light : Color = None, lights : list[Light] = None):
+                obj_list : list[Object],
+                ambient_light : Color = None, lights : list[Light] = None,
+                max_depth = 0):
         self.width = width
         self.height = height
         self.cam_pixel_size = cam_pixel_size
@@ -35,3 +38,4 @@ class Scene:
         self.obj_list = obj_list
         self.ambient_light = ambient_light
         self.lights = lights
+        self.max_depth = max_depth
